ea8aaf35a9b7f723d3bea2f66205a74f86e5e297
Mohit Verma <mohit@Mohits-MacBook-Air.local>
2025-01-02 21:32:39 +0530

    Updated : User validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,10 +4,10 @@ const userService = require("../services/userService");
 const { validationResult } = require("express-validator");
 const { config } = require("../config");
 module.exports.registerUser = async (req, res, next) => {
-  // const errors = validationResult(req);
-  // if (!errors.isEmpty()) {
-  //   return res.status(400).json({ ok: false, error: errors.array() });
-  // }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ ok: false, error: errors.array() });
+  }
   try {
     const { fullname, email, password } = req.body;
     const existingUser = await userModel.findOne({ email });
@@ -43,10 +43,10 @@ module.exports.registerUser = async (req, res, next) => {
 };
 
 module.exports.loginUser = async (req, res, next) => {
-  // const errors = validationResult(req);
-  // if (!errors.isEmpty()) {
-  //   return res.status(400).json({ ok: false, error: errors.array() });
-  // }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ ok: false, error: errors.array() });
+  }
   try {
     const { email, password } = req.body;
     const user = await userModel.findOne({ email }).select("+password");
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,9 +5,30 @@ const UserController = require("../controllers/userController");
 
 const authMiddleware = require("../middlewares/authMiddleware");
 
-router.post("/register", UserController.registerUser);
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("fullname.firstname")
+      .isLength({ min: 3 })
+      .withMessage("First name must be atleast 3 characters long"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be atleast 6 characters long"),
+  ],
+  UserController.registerUser
+);
 
-router.post("/login", UserController.loginUser);
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be atleast 6 characters long"),
+  ],
+  UserController.loginUser
+);
 router.get("/auth", authMiddleware.authUser, UserController.authenticateUser);
 router.get("/", authMiddleware.authUser, UserController.getUserProfile);
 router.get("/logout", authMiddleware.authUser, UserController.logoutUser);
