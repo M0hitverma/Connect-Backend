08e8db0f7edbdf0e7f291f40121dcfb304f2e1b8
Mohit Verma <mohit@Mohits-MacBook-Air.local>
2025-01-02 21:27:08 +0530

    Updated : userController

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,36 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {body}= require('express-validator');
-const UserController = require('../controllers/userController');
+const { body } = require("express-validator");
+const UserController = require("../controllers/userController");
 
-const authMiddleware = require('../middlewares/authMiddleware');
+const authMiddleware = require("../middlewares/authMiddleware");
 
+router.post("/register", UserController.registerUser);
 
-router.post("/register",
-    [
-      body("email").isEmail().withMessage("Invalid Email"),
-      body("fullname.firstname")
-        .isLength({ min: 3 })
-        .withMessage("First name must be atleast 3 characters long"),
-      body("password")
-        .isLength({ min: 6 })
-        .withMessage("Password must be atleast 6 characters long"),
-    ],
-    UserController.registerUser
-  );
-  
-  router.post("/login",
-    [
-      body("email").isEmail().withMessage("Invalid Email"),
-      body("password")
-        .isLength({ min: 6 })
-        .withMessage("Password must be atleast 6 characters long"),
-    ],
-    UserController.loginUser
-  );
-  router.get("/auth", authMiddleware.authUser, UserController.authenticateUser);
-  router.get("/",authMiddleware.authUser,UserController.getUserProfile);
-  router.get("/logout", authMiddleware.authUser, UserController.logoutUser);
-  router.get("/posts",authMiddleware.authUser,UserController.getUserPost);
-  module.exports = router;
-  
+router.post("/login", UserController.loginUser);
+router.get("/auth", authMiddleware.authUser, UserController.authenticateUser);
+router.get("/", authMiddleware.authUser, UserController.getUserProfile);
+router.get("/logout", authMiddleware.authUser, UserController.logoutUser);
+router.get("/posts", authMiddleware.authUser, UserController.getUserPost);
+module.exports = router;
